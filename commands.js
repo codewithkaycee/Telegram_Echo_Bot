e80816e7fb32766497b1ef8de7a928fad41bee40
Kaycee Nuel <heykayceee@gmail.com>
2023-07-04 21:45:05 +0100

    Made a change in the command features

--- commands.js
+++ commands.js
@@ -1,29 +1,29 @@
-//still working on this feature
-const axios = require('axios')
-const { BotAPI } = require ('./config')
+// //still working on this feature
+// const axios = require('axios')
+// const { BotAPI } = require ('./config')
 
-const commandHandle = async(chatId,commands) => {
-    if (commands === '/start'){
-        await axios.post(`${BotAPI}/sendMessage`,{
-            chat_id: chatId,
-            text: 'Welcome to the bot!'
-        });
-    }else if(commands === '/help'){
-        await axios.post(`${BotAPI}/sendMessage`,{
-            chat_id: chatId,
-            text: 'How can I help you?'
-        });
-    }else if (commands === '/settings'){
-        await axios.post(`${BotAPI}/sendMessage`, {
-            chat_id : chatId,
-            text : 'Edit Bot'
-        })
-    }else{
-        await axios.post(`${BotAPI}/sendMessage`,{
-            chat_id : chatId,
-            text: 'Unknown Command.'
-        })
-    }
-};
+// const commandHandle = async(chatId,commands) => {
+//     if (commands === '/start'){
+//         await axios.post(`${BotAPI}/sendMessage`,{
+//             chat_id: chatId,
+//             text: 'Welcome to the bot!'
+//         });
+//     }else if(commands === '/help'){
+//         await axios.post(`${BotAPI}/sendMessage`,{
+//             chat_id: chatId,
+//             text: 'How can I help you?'
+//         });
+//     }else if (commands === '/settings'){
+//         await axios.post(`${BotAPI}/sendMessage`, {
+//             chat_id : chatId,
+//             text : 'Edit Bot'
+//         })
+//     }else{
+//         await axios.post(`${BotAPI}/sendMessage`,{
+//             chat_id : chatId,
+//             text: 'Unknown Command.'
+//         })
+//     }
+// };
 
-module.exports = commandHandle
+// module.exports = commandHandle
